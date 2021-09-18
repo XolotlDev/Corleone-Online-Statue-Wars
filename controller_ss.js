595d4ce56909bdd24310203f5c04867bdb7cfe87
XolotlDev <83152377+XolotlDev@users.noreply.github.com>
2021-09-17 22:10:16 -0400

    Update controller_ss.js

--- controller_ss.js
+++ controller_ss.js
@@ -1,6 +1,6 @@
 /* 
    Statue Wars, Written by XolotlDev & 0x0 
-   Pre-Alpha Version 1.0, 
+   Version 1.0, 
    Last Date Modified by Xol: 9/15/2021 @ 1:34 PM EST
    Last Editor: Xol
    
@@ -9,69 +9,100 @@
 
 // Initialize Controller
 function onUpdated() {
-	let offset = "    "; // string padding offset
+	let strPadding = "    "; // string padding offset
 	this.x = 21.5;
 	this.y = 26;
 	this.zoom = 1.5;
 	this.drawunder = true;
 	this.nonblocking = true;
-	this.name = offset + "[Controller V2]";
-	this.debug = false;
+	this.name = strPadding + "[Controller V2]";
+	this.debug = false; //shh
 	this.scheduleevent(0, "setstatueproperties", pl);
 	this.say("Controller Initialized!");
 }
 
 function onPlayerSays(pl) {
 	// Allows the help command for non admin Players
-	if (pl.chat.toLowerCase() == "statue help") this.triggerclient(pl, "showhelpgui");
+	if (pl.chat.toString().toLowerCase() == "statue help") {
+		pl.chat = "";
+		this.triggerclient(pl, "showhelpgui");
+	}
 
-    // Suggest correct command usage rip off
-    let plrCmds = "<b>Available commands:</b> statue help";
-    let eAdminCmds = "<b>Event Admin commands:</b> statue start, statue stop, statue reset, statue zoom"; 
-    let dAdminCmds = "<b>Development Admin commands:</b> statue ping, statue position, statue roll, statue lock"; 
-    if (pl.chat.toLowerCase() == "statue") {
-        if (pl.clanname.includes("Events Admin") && pl.adminlevel > 0) pl.showmessage(plrCmds + " <br>" + eAdminCmds);
-        else if (pl.clanname.includes("Development Admin") && pl.adminlevel > 0) pl.showmessage(plrCmds + " <br>" + eAdminCmds + " <br>" + dAdminCmds);
-        else pl.showmessage(plrCmds);
-    }
-    
+	// Suggest correct command usage rip off
+	let plrCmds = "<b>Available Commands:</b> statue help";
+	let eAdminCmds = "<b>Event Admin Commands:</b> statue start, statue stop, statue reset, statue zoom";
+	let dAdminCmds = "<b>Development Admin Commands:</b> statue ping, statue position, statue roll, statue lock";
+	if (pl.chat.toString().toLowerCase() == "statue") {
+		if (pl.clanname.toString().includes("Events Admin") && pl.adminlevel > 0) pl.showmessage(plrCmds + " <br>" + eAdminCmds);
+		else if (pl.clanname.toString().includes("Development Admin") && pl.adminlevel > 0) pl.showmessage(plrCmds + " <br>" + eAdminCmds + " <br>" + dAdminCmds);
+		else pl.showmessage(plrCmds);
+	}
+
 	// Prevent non admin Players from accessing commands
-	if (pl.adminlevel > 0 && !pl.clanname.includes("Admin") && 
-	pl.chat.includes("statue") && pl.chat.length > 6) pl.showmessage("Insufficient Permissions");
-	
-	if (!pl.clanname.includes("Admin") || pl.adminlevel == 0) return;
+	if (pl.adminlevel > 0 && !pl.clanname.toString().includes("Admin") &&
+		pl.chat.includes("statue") && pl.chat.length > 6) pl.showmessage("Insufficient Permissions");
+
+	if (!pl.clanname.toString().includes("Admin") || pl.adminlevel == 0) return;
 
 	// Construct staff commands
-	switch (pl.chat.toLowerCase()) // convert player chat to lowercase to prevent casing issues with cmds
+	switch (pl.chat.toString().toLowerCase()) // convert player chat to lowercase to prevent casing issues with cmds
 	{
 		case "statue start": // Brings up the client side GUI for the admin Player
+			pl.chat = "";
 			this.scheduleevent(0, "sendinstruction", pl, "start");
 			break;
 		case "statue reset": // Resets statue properties to default
+			pl.chat = "";
 			this.scheduleevent(0, "sendinstruction", pl, "reset");
 			break;
 		case "statue zoom": // Enlarges or shrinks statues by x2
+			pl.chat = "";
 			this.scheduleevent(0, "sendinstruction", pl, "zoom");
 			break;
 		case "statue position": // Repositions statue's to their correct positions 
+			pl.chat = "";
 			this.scheduleevent(0, "sendinstruction", pl, "position");
+			break;
+		case "statue survivors":
+			this.say("Survivors: " + onGetInfo("players"));
 			break;
 	}
 
 	// Prevent non development admin Players from accessing commands
-	if (!pl.clanname.includes("Development Admin") || pl.adminlevel == 0) return;
+	if (pl.adminlevel > 0 && !pl.clanname.toString().includes("Development Admin") &&
+		pl.chat.toString().includes("statue") && pl.chat.length > 6) pl.showmessage("Insufficient Permissions");
 
+	if (!pl.clanname.toString().includes("Development Admin") || pl.adminlevel == 0) return;
 
 	// Construct development staff commands
-	switch (pl.chat.toLowerCase()) {
+	switch (pl.chat.toString().toLowerCase()) {
 		case "statue ping": // Debugging command used for checking that all statue npcs are operating
+			pl.chat = "";
 			this.scheduleevent(0, "sendinstruction", pl, "ping");
 			break;
 		case "statue lock":
-			this.scheduleevent(0, "lockstatues", pl, "", true);
+			pl.chat = "";
+			this.scheduleevent(0, "lockstatues", pl, null, true);
 			break;
 		case "statue roll":
+			pl.chat = "";
 			this.scheduleevent(0, "rollstatues");
+			break;
+		case "statue unlock":
+			pl.chat = "";
+			this.scheduleevent(0, "lockstatues", pl, null, false);
+			break;
+		case "statue test":
+			pl.chat = "";
+			
+			/*
+			let Survivors = [];
+			onGetInfo("statues").forEach((statue) => {
+				Survivors.push(statue.tag);
+			});
+			this.say("Survivors: " + Survivors); */
+			
+			this.scheduleevent(0, "lockstatues", pl, "      Statue #11", false);
 			break;
 	}
 
@@ -95,6 +126,7 @@ function onSendInstruction(pl, type, data) {
 			break;
 		case "reset":
 			this.scheduleevent(0, "resetstatues", pl);
+			this.scheduleevent(0, "namestatues", pl);
 			break;
 		case "zoom":
 			this.scheduleevent(0, "zoomstatues");
@@ -137,9 +169,20 @@ function onResetStatues(pl) {
 		statue.hat = pl.hat;
 		statue.head = pl.head;
 		statue.body = pl.body;
-		statue.ani = "player_idle";
+		statue.ani = "player_idle[1]";
 		statue.zoom = 1;
 		statue.name = "";
+		
+	  /*	let statueOwnerTag = statue.tag.substring(
+            statue.tag.indexOf("(") + 1, 
+            statue.tag.lastIndexOf(")")
+        );
+        
+        let statueOwner;
+		if (statue.tag.includes("("))  statueOwner = statue.tag.substring(0, statue.tag.length - 5 - 2);
+		else statueOwner = pl.tag;
+		
+	*/	
 		statue.chat = "";
 
 		onGetInfo("players").forEach((plr) => {
@@ -158,7 +201,53 @@ function onZoomStatues() {
 
 // Start statue wars event
 function onClientStartStatues(pl) {
+	let startDelay = 5;
+	let maxStatues = 12;
+	let lockAmount = maxStatues - onGetInfo("players").length;
+	var _lockAmount = lockAmount;
 
+	/*
+	 Summary: Start off with locking all statues, I don't do this before all players 
+	 are on the level because its easier to update their clients then.
+	*/
+
+	this.scheduleevent(0, "lockstatues", pl, null, true); // Lock Statues
+
+	let strPadding = "      ";
+	let currentStatue = strPadding + "Statue #" + _lockAmount.toString();
+
+	if (onGetInfo("players").length < maxStatues) { // Less than 12 players
+		onGetInfo("statues").forEach((statue) => {
+			if (statue.name == currentStatue) { // Only unlock statues we need unlocked
+				this.scheduleevent(startDelay + 1, "lockstatues", pl, currentStatue, false); // + 1 to account for 1 second for thread sleep delay
+				_lockAmount--;
+			}
+		});
+	} else this.scheduleevent(startDelay + 1, "lockstatues", pl, null, false); // Unlock all statues 
+    
+	this.scheduleevent(0, "runtimer", startDelay, "Unlocking Statues"); // Timer
+
+}
+
+// Starts timer
+function onRunTimer(timer, msg) {
+	let Color;
+	for (var i = timer; i > 0; i--) {
+		switch (timer) {
+			case 5:
+				Color = "lime";
+				break;
+			case 3:
+				Color = "yellow";
+				break;
+			case 1:
+				Color = "red";
+				break;
+		}
+		this.showhp(msg + "(" + timer.toString() + ")", Color);
+		timer--;
+		this.sleep(1);
+	}
 }
 
 // Stop statue wars event
@@ -173,12 +262,12 @@ function onPauseStatues(pl) {
 
 // Returns a random statue
 function onRollStatues() {
-    let rollColor = [2.5,2.5,1,2.5];
-    var time = 0;
-    
+	let rollColor = [2.5, 2.5, 1, 2.5];
+	var time = 0;
+
 	onGetInfo("statues").forEach((statue) => {
 		statue.scheduleevent(time, "rollstatue", onGetInfo("players"), 0.5, rollColor);
-		time++
+		time++;
 	});
 }
 
@@ -189,15 +278,20 @@ function onLockStatues(pl, statue, bool) {
 	   in the 'statue' argument if not then every statue gets locked foreach 
 	   player on the level.
 	*/
-
+	
 	onGetInfo("statues").forEach((_statue) => {
-		if (_statue == statue) this.triggerclient(plr, "lockstatues", true, _statue);
-		else if (statue == "") onGetInfo("players").forEach((plr) => {
-			if (bool) _statue.scheduleevent(0, "lockstatue", plr);
-			else _statue.scheduleevent(0, "unlockstatue", plr);
+		onGetInfo("players").forEach((plr) => {
+			if (_statue.name == statue) {
+				if (bool) _statue.scheduleevent(0, "lockstatue", plr);
+				else _statue.scheduleevent(0, "unlockstatue", plr);
+			} else if (statue == "" || statue == null) {
+				if (bool) _statue.scheduleevent(0, "lockstatue", plr);
+				else _statue.scheduleevent(0, "unlockstatue", plr);
+			}
 		});
 	});
 }
+
 
 // Grabs and returns specified event info
 function onGetInfo(type, npc) {
@@ -212,7 +306,7 @@ function onGetInfo(type, npc) {
 			}
 		})
 		.filter(function(npc) {
-			return !npc.name.includes("Controller") && !npc.name.includes("Test");
+			return !npc.name.includes("Controller") && !npc.name.toLowerCase().includes("test");
 		});
 
 	// Grab all unnamed statue NPCs
@@ -228,6 +322,21 @@ function onGetInfo(type, npc) {
 		.filter(function(npc) {
 			return npc.name.length == 0;
 		});
+	
+	// Grab all named statue NPCs
+	let namedstatues = Server.searchnpcs({
+			map: this.map,
+			area: {
+				x: 4,
+				y: 11,
+				w: 300,
+				h: 300
+			}
+		})
+		.filter(function(npc) {
+			return npc.name.includes("Statue #");
+	});
+
 
 	// Grab all unlocked statue NPCs
 	let unlockedstatues = Server.searchnpcs({
@@ -248,7 +357,7 @@ function onGetInfo(type, npc) {
 	let players = Server.searchPlayers({
 		map: this.map
 	}).filter(function(plrs) {
-		return !plrs.clanname.includes("Adm1in");
+		return !plrs.clanname.toString().includes("Adm1in");
 	});
 
 	/*
@@ -262,18 +371,17 @@ function onGetInfo(type, npc) {
     
 	/ Filter survivors through Player IDs
 	survivors = players.filter(function(plr){
-	    return survivors.includes(plr.name)
+	    return survivors.toString().includes(plr.name)
 	});
     
 	*/
 
 	// Grab a list of Players to kick from the event
 	let toKick = players.filter(function(plr) {
-		return !survivors.includes(plr.name); // Grab every player who didn't survive
+		return !survivors.toString().includes(plr.name); // Grab every player who didn't survive
 	});
 
-    npc.sort((a,b) => a-b);
-    
+
 	// Debugging stuff
 	if (this.debug) pl.echo(this.name.toUpperCase() + ": Event -> onGetInfo(), NPC ID -> " + this.id + ", Players -> " + players + ", Survivors -> " + survivors);
 
@@ -288,6 +396,8 @@ function onGetInfo(type, npc) {
 			return unnamedstatues;
 		case "unlockedstatues":
 			return unlockedstatues;
+		case "namedstatues":
+		    return namedstatues;
 	}
 
 }
